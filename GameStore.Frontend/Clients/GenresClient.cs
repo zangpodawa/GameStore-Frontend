383e0fa396ff0db225382754721875b3303495a7
Dawa <dzangpo29@gmail.com>
2024-11-24 21:10:44 +1000

    First commit

---
+++ GameStore.Frontend/Clients/GenresClient.cs
@@ -0,0 +1,10 @@
+using System;
+using GameStore.Frontend.Models;
+
+namespace GameStore.Frontend.Clients;
+
+public class GenresClient(HttpClient httpClient)
+{
+  public async Task<Genre[]> GetGenresAsync() 
+    => await httpClient.GetFromJsonAsync<Genre[]>("genres") ?? [];
+}
---
+++ GameStore.Frontend/Models/Genre.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace GameStore.Frontend.Models;
+
+public class Genre
+{
+  public int Id { get; set;}
+  public required string Name { get; set;}
+}
